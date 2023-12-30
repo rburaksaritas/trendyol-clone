48dc09a4778f3e7cdf7fd32ab1e0b1a293217916
burak <rburaksaritas@gmail.com>
2023-12-30 16:06:52 +0300

    Allow CORS

---
+++ backend/src/main/kotlin/com/trendyolclone/backend/config/WebConfig.kt
@@ -0,0 +1,23 @@
+package com.trendyolclone.backend.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig() : WebMvcConfigurer {
+
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+            }
+        }
+    }
+}
