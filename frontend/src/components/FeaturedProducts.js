fce195942877bb8c297696266f16fca188a9aca0
burak <rburaksaritas@gmail.com>
2024-01-02 15:25:25 +0300

    Add container for featured products

---
+++ frontend/src/components/FeaturedProducts.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ProductCard from './ProductCard';
+import './FeaturedProducts.css';
+
+const FeaturedProducts = ({ title, products }) => {
+  return (
+    <div className="featured-products">
+      <h3>{title}</h3>
+      <div className="products-container">
+        {products.map(product => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedProducts;
