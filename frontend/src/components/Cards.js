ca151915bd8d8351000913e466764e67c731dfb0
burak <rburaksaritas@gmail.com>
2024-01-03 10:12:12 +0300

    Add searching via cards

--- frontend/src/components/Cards.js
+++ frontend/src/components/Cards.js
@@ -1,13 +1,19 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import './Cards.css';
 
 const Cards = ({cards}) => {
+
+  const navigate = useNavigate();
+  const handleCardClick = (categoryName) => {
+    navigate(`/search/${categoryName.toLowerCase()}`); 
+  };
 
   return (
     <div className="cards-container">
       {cards.length > 0 ? (
         cards.map((card) => (
-          <div key={card.id} className="card" onClick={() => console.log('Card clicked!', card)}>
+          <div key={card.id} className="card" onClick={() => handleCardClick(card.category)}>
             <img src={card.imageUrl} alt={card.title} className="card-image" />
             <div className="card-title">{card.title}</div>
           </div>
