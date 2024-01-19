58af00cb801d5b9d6e799d595ef9137f5dec2c21
burak <rburaksaritas@gmail.com>
2024-01-19 09:15:04 +0300

    Update lazy load

--- frontend/src/components/Products.js
+++ frontend/src/components/Products.js
@@ -5,54 +5,54 @@ import './Products.css';
 
 const Products = () => {
   const [products, setProducts] = useState([]);
-  const [page, setPage] = useState(1);
-  const [hasMore, setHasMore] = useState(true);
+  const [displayCount, setDisplayCount] = useState(40); // Initial number of products to display
   const { category } = useParams();
-  const pageSize = 20; // Number of products to load per page
 
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        let url = `${process.env.REACT_APP_BACKEND_URL}/products/search?sortBy=reviewsCount&direction=DESC&size=${pageSize}&page=${page}`;
+        let url = `${process.env.REACT_APP_BACKEND_URL}/products/search?sortBy=reviewsCount&direction=DESC`;
         if (category) {
-          url += `&searchQuery=${category}`;
+          if (category === "çok satanlar") {
+            url += `&size=200`;   
+          } else if (category === "flaş ürünler") {
+            url += `&size=62&searchQuery=yeni`;  
+          } else {
+            url += `&size=10000&searchQuery=${category}`;
+          }
+        } else {
+          url += `&size=62&page=1`;
         }
         const response = await fetch(url);
         if (!response.ok) {
           throw new Error('Network response was not ok');
         }
         const data = await response.json();
-        setProducts(prevProducts => [...prevProducts, ...data]);
-        setHasMore(data.length === pageSize);
+        setProducts(data);
       } catch (error) {
         console.error('Fetch error:', error);
       }
     };
+    
+    setDisplayCount(40);
+    fetchProducts();
+  }, [category]);
 
-    if (hasMore) {
-      fetchProducts();
-    }
-  }, [category, page]);
-
-  // Function to handle scrolling near the bottom of the page
-  const loadMore = () => {
-    setPage(prevPage => prevPage + 1);
+  const loadMoreProducts = () => {
+    setDisplayCount(currentCount => currentCount + 40); // Load 20 more products
   };
-
-  // Add event listener for scrolling or use a library component here
-  // For example, you can use an infinite scroll component here
 
   return (
     <div className="products">
       <h3>{category ? `"${category}" için ${products.length} sonuç` : ''}</h3>
       <div className="products-container">
-        {products.map(product => (
+        {products.slice(0, displayCount).map(product => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
-      {hasMore && (
-          <button className="load-more-btn" onClick={loadMore}>Daha Fazla</button>
-        )}
+      {displayCount < products.length && (
+        <button className="load-more-btn" onClick={loadMoreProducts}>Daha Fazla</button>
+      )}
     </div>
   );
 };
